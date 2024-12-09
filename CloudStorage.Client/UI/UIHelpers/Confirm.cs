e9fb054ed64147a08467dadd89b9e30ee891e3c5
diamond <diamond@DESKTOP-C0LRN55>
2024-12-09 22:54:57 +0400

    Back for Deleting

--- CloudStorage.Client/UI/UIHelpers/Confirm.cs
+++ CloudStorage.Client/UI/UIHelpers/Confirm.cs
@@ -1,5 +1,7 @@
 ﻿using CloudStorage.Client.UI.Modals;
 using CloudStorage.Client.Utils;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace CloudStorage.Client.UI.UIHelpers;
 
@@ -9,6 +11,22 @@ public static class Confirm
     {
         var conf = new Confirmation(title, yesbtn, nobtn);
         conf.Owner = WindowUtils.ActiveWindow;
+        
+        var grid = WindowUtils.ActiveWindow?.Content as Grid;
+        if(grid != null)
+        {
+            var border = new Grid();
+            border.Background = Brushes.LightGray;
+            border.Opacity = 0.5;
+            Grid.SetZIndex(border, 1000);
+            grid.Children.Add(border);
+
+            var result = conf.ShowDialog() ?? false;
+
+            grid.Children.Remove(border);
+            return result;
+        }
+
         return conf.ShowDialog() ?? false;
     }
 }
